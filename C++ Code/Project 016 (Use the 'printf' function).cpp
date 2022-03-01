a3640fc5b33f53c5c7cdd44af7d32e5f8acedf8c
Alexandre Perreault <epsilons@hotmail.com>
2022-03-01 03:13:31 -0500

    Create Project 016 (Use the 'printf' function).cpp

---
+++ C++ Code/Project 016 (Use the 'printf' function).cpp
@@ -0,0 +1,14 @@
+// Do a research on the printf function.Explain where this function comes fromand use it to do a Hello World.
+// The printf function (the name comes from "print formatted") prints a string to the screen.
+
+
+#include <iostream>
+
+int main()
+{
+    printf("Hello World!");
+
+    system("pause");
+
+    return 0;
+}
