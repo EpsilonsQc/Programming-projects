f63207e8d8bf574794afbf1aeaddb21e2cfe78f2
Alexandre Perreault <epsilons@hotmail.com>
2022-03-01 01:33:36 -0500

    Update Project 5.cpp

--- C++ Code/Project 5.cpp
+++ C++ Code/Project 5.cpp
@@ -1,3 +1,5 @@
+//Write a program that displays the numbers 1 through 4 on the same line, separated by spaces.Use a single output statementand a single insertion operator.
+
 #include <iostream>
 
 using namespace std;
