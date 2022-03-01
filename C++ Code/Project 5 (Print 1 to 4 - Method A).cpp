8df7324922f19656935403b147506ca899d90c26
Alexandre Perreault <epsilons@hotmail.com>
2022-03-01 01:55:10 -0500

    Update and rename Project 5.cpp to Project 5 (Print 1 to 4 - Method A).cpp

---
+++ C++ Code/Project 5 (Print 1 to 4 - Method A).cpp
@@ -0,0 +1,15 @@
+// Displays the numbers 1 through 4 on the same line, separated by spaces.Use a single output statementand a single insertion operator.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "1 2 3 4";
+
+    system("pause");
+
+    return 0;
+}
