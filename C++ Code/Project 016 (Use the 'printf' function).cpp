6d07f76664e426bba2f2a35672dba12cb30ba5fb
Alexandre Perreault <epsilons@hotmail.com>
2022-03-01 22:55:10 -0500

    Update Project 016 (Use the 'printf' function).cpp

--- C++ Code/Project 016 (Use the 'printf' function).cpp
+++ C++ Code/Project 016 (Use the 'printf' function).cpp
@@ -1,6 +1,6 @@
 // Do a research on the printf function and explain where this function comes fromand use it to do a Hello World.
 // The printf function (the name comes from "print formatted") prints a string to the screen. 
-// This function is an inheritance from the C programming language which is available in C library by default
+// This function is an inheritance from the C programming language and is available in C library by default
 
 #include <iostream>
 
