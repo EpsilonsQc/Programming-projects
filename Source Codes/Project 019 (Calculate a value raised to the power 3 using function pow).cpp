a3c9a5869ce450f8f17fc4dc9e6f03ec6cef285f
Alexandre Perreault <epsilons@hotmail.com>
2022-03-30 23:21:11 -0400

    Add files via upload

---
+++ Source Codes/Project 019 (Calculate a value raised to the power 3 using function pow).cpp
@@ -0,0 +1,16 @@
+// Calculate the value of 2.5 raised to the power 3 using function pow. 
+// Print the result with a precision of 2 in a field width of 10 positions.
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+        cout << fixed << setprecision(2) << setw(10) << pow(2.5, 3) << endl << endl;
+
+    system("pause");
+
+    return 0;
+}
---
+++ Source Codes/Project 023 (Print a table that shows the value of π).cpp
@@ -0,0 +1,57 @@
+﻿// Calculating the Value of π from the infinite series
+//
+// π = 4 - 4/3 + 4/5 - 4/7 + 4/9 - 4/11 + …
+//
+// Print a table that shows the value of π approximated by computing the first 200,000 terms of this series
+// Show how many terms are used before getting a value that begins with 3.14159
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int calculatePi(); // prototype de fonction [déclare l'existance d'une fonction]
+
+int main() // fonction main
+{
+    int howMany = calculatePi(); // appel de la fonction calculatePi() dans le main
+
+    cout << endl << "The value " << "'3.14159'" << " was achieved after a total of " << howMany << " operations." << endl << endl;
+
+    system("pause");
+
+    return 0;
+}
+
+int calculatePi() // fonction suivit de son body
+{   
+    int maxOpCount = 200000;
+    int lineNumber = 1;
+    int divValue = 3;
+    bool flag = false;
+    double pi = 4.0;
+
+    for (int i = 1; i <= maxOpCount; ++i)
+    {
+        if (i % 2 == 0)
+        {
+            pi += 4.0 / divValue;
+            divValue += 2;
+        }
+        else
+        {
+            pi -= 4.0 / divValue;
+            divValue += 2;
+        }
+
+        cout << "Line # " << i << " -> " << setprecision(10) << pi << endl;
+
+        if ((static_cast<int>(pi * 100000) == 314159) && !flag)
+        {
+            lineNumber = i;
+            flag = true;
+        }
+    }
+
+    return lineNumber; // ce que la fonction retourne
+}
