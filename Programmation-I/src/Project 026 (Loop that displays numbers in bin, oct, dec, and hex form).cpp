42e5c229e8e618ec3d65b3425fe81703a892f28b
Alexandre Perreault <epsilons@hotmail.com>
2022-04-01 02:38:26 -0400

    Add files via upload

--- Programmation-I/src/Project 026 (Loop that displays numbers in bin, oct, dec, and hex form).cpp
+++ Programmation-I/src/Project 026 (Loop that displays numbers in bin, oct, dec, and hex form).cpp
@@ -1,4 +1,4 @@
-// A loop that displays the numbers from 1 to 256 in decimal, octal, hexadecimal and binary form.
+// A loop that displays the numbers from 1 to 256 in hexadecimal, decimal, octal and binary form.
 
 #include <iostream>
 #include <bitset>
@@ -11,18 +11,18 @@ int main()
 
     for (unsigned int outputValue = 1; outputValue <= 256; ++outputValue)
     {
-        cout << "Decimal     : " << dec << outputValue << endl;                                
-        cout << "Hexadecimal : " << hex << outputValue << endl;                                
+        cout << "Hexadecimal : " << hex << outputValue << endl;
+        cout << "Decimal     : " << dec << outputValue << endl;
         cout << "Octal       : " << oct << outputValue << endl;
 
         binaryChunk = bitset<12>(outputValue).to_string(); // ASSIGN THE BINARY NUMBER TO A STRING
-        binaryChunk.insert(4, 1, ' '); // ADD AN EMPTY SPACE ONE TIME AT POSITION 4
-        binaryChunk.insert(9, 1, ' '); // ADD AN EMPTY SPACE ONE TIME AT POSITION 9
+        binaryChunk.insert(4, 1, ' '); // ADD AN EMPTY SPACE ONE TIME AT POSITION 4 (CREATE CHUNK 1)
+        binaryChunk.insert(9, 1, ' '); // ADD AN EMPTY SPACE ONE TIME AT POSITION 9 (CREATE CHUNK 2)
 
         cout << "Binary      : " << binaryChunk << endl << endl;
     }
 
-system("pause");
+    system("pause");
 
-return 0;
+    return 0;
 }
