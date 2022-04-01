a797d18f7a604b06c4acf60eaf4fb02523444128
Alexandre Perreault <epsilons@hotmail.com>
2022-04-01 02:00:26 -0400

    Add files via upload

--- Programmation-I/src/Project 026 (Loop that displays numbers in bin, oct, dec, and hex form).cpp
+++ Programmation-I/src/Project 026 (Loop that displays numbers in bin, oct, dec, and hex form).cpp
@@ -1,47 +1,28 @@
 // A loop that displays the numbers from 1 to 256 in decimal, octal, hexadecimal and binary form.
 
 #include <iostream>
+#include <bitset>
 
 using namespace std;
 
 int main()
 {
+    string binaryChunk = "";
 
-    unsigned int maxNumbers = 256;
-
-    for (unsigned int outputValue = 1; outputValue <= maxNumbers; outputValue++)
+    for (unsigned int outputValue = 1; outputValue <= 256; ++outputValue)
     {
-        cout << "hexadecimal : " << hex << outputValue << endl;
-        cout << "octal       : " << oct << outputValue << endl;
-        cout << "decimal     : " << dec << outputValue << endl;
+        cout << "Decimal     : " << dec << outputValue << endl;                                
+        cout << "Hexadecimal : " << hex << outputValue << endl;                                
+        cout << "Octal       : " << oct << outputValue << endl;
 
-        // START OF THE DECIMAL TO BINARY OPERATIONS
-        cout << endl << "DEBUG  : maxNumbers print  : " << maxNumbers << endl; // DEBUG LINE
-        cout << "DEBUG  : outputValue print : " << outputValue << endl; // DEBUG LINE
+        binaryChunk = bitset<12>(outputValue).to_string(); // ASSIGN THE BINARY NUMBER TO A STRING
+        binaryChunk.insert(4, 1, ' '); // ADD AN EMPTY SPACE ONE TIME AT POSITION 4
+        binaryChunk.insert(9, 1, ' '); // ADD AN EMPTY SPACE ONE TIME AT POSITION 9
 
-        unsigned int number = 0;
-        unsigned int binary = 0;
-
-        number = outputValue; // ASSIGN outputValue [1 to 256] value to number
-        cout << "DEBUG  : number (outputValue assigned) : " << number << endl; // DEBUG LINE
-
-        while (number != 0)
-        { 
-            binary = number % 2; // MODULO 2 [NOT WORKING FOR UNKNOWN REASON]
-            cout << "DEBUG  : binary = number % 2 (modulo 2) : " << number << endl; // DEBUG LINE
-
-            number /= 2; // DIV BY 2
-            cout << "DEBUG  : number divided by 2 : " << number << endl; // DEBUG LINE
-
-            cout << endl << "Binary : " << binary << endl; // SUPPOSED TO OUTPUT BINARY NUMBER
-        }
-        // END OF THE DECIMAL TO BINARY OPERATIONS
-
-        cout << endl << endl;
-
+        cout << "Binary      : " << binaryChunk << endl << endl;
     }
 
-    system("pause");
+system("pause");
 
-    return 0;
+return 0;
 }
