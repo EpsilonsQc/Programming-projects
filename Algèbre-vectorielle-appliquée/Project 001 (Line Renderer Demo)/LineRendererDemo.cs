a31277530f8780d88449270a52f367391e802337
Alexandre Perreault <epsilons@hotmail.com>
2022-05-07 22:54:40 -0400

    Add files via upload

---
+++ Algèbre-vectorielle-appliquée/Project 001 (Line Renderer Demo)/LineRendererDemo.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererDemo : MonoBehaviour
+{
+    [SerializeField] private LineRenderer myLineRenderer = default;
+    [SerializeField] private Vector2 point1 = new Vector2(-20, -16); // (coordonée x, y)
+    [SerializeField] private Vector2 point2 = new Vector2(20, 16); // (coordonée x, y)
+
+    void Awake()
+    {
+        myLineRenderer = gameObject.GetComponent<LineRenderer>();
+        Debug.Assert(myLineRenderer != null, "<WARNING: Pas de lineR");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLineRenderer.positionCount = 2;
+        myLineRenderer.widthMultiplier = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetPositions();
+    }
+
+    // SetPositions function
+        private void SetPositions()
+    {
+        myLineRenderer.SetPosition(0, point1);
+        myLineRenderer.SetPosition(1, point2);
+    }
+} 
