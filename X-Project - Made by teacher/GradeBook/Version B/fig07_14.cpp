b154064c23ae893a7be13268220dd3e30471956a
Alexandre Perreault <epsilons@hotmail.com>
2022-05-08 01:41:05 -0400

    Add files via upload

---
+++ X-Project - Made by teacher/GradeBook/Version B/fig07_14.cpp
@@ -0,0 +1,36 @@
+// Fig. 7.14: fig07_14.cpp
+// Creates GradeBook object using an array of grades.
+#include <array>
+#include "GradeBook.h" // GradeBook class definition
+//using namespace std;
+
+int main() {
+   // array of student grades
+   const std::array<int, GradeBook::NB_STUDENTS> grades = {87, 68, 94, 100, 83, 78, 85, 91, 76, 87};
+
+   std::string courseName = "CS101 Introduction to C++ Programming";
+
+   GradeBook myGradeBook(courseName, grades);
+   myGradeBook.getCourseName();
+   myGradeBook.displayMessage();
+   myGradeBook.processGrades();
+
+   GradeBook::a = 99;
+}
+
+
+
+/**************************************************************************
+ * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ **************************************************************************/
