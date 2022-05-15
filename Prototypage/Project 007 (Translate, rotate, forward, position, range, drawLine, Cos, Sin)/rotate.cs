bc85354c4d998aec3b670c3426687d1b2abe4c1f
Alexandre Perreault <epsilons@hotmail.com>
2022-05-15 00:21:18 -0400

    Update rotate.cs

--- Prototypage/Project 007 (Translate, rotate, forward, position, range, drawLine, Cos, Sin)/rotate.cs
+++ Prototypage/Project 007 (Translate, rotate, forward, position, range, drawLine, Cos, Sin)/rotate.cs
@@ -9,7 +9,7 @@ public class rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the attached gameObject (cube) of +1 Z-Axis on each frame update
+        // Rotate the attached gameObject (cube) of +1 Y-Axis on each frame update
         transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * rotateSpeed);
     }
 }
