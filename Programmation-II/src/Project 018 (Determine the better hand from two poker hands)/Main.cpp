97ec9a55ecbd0e07c28ef99b478e2abcce638693
Alexandre Perreault <epsilons@hotmail.com>
2022-05-16 23:28:05 -0400

    Add files via upload

---
+++ Programmation-II/src/Project 018 (Determine the better hand from two poker hands)/Main.cpp
@@ -0,0 +1,15 @@
+// Determine the better hand from two poker hands
+// Main.cpp (Main function | Program execution begins and ends there) 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+	system("pause");
+
+	return 0;
+}
