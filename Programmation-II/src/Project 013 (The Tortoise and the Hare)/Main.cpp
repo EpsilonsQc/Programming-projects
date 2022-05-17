9934784e3e9a1d01e01a03d9791f0492aae413e3
Alexandre Perreault <epsilons@hotmail.com>
2022-05-16 23:25:21 -0400

    Add files via upload

--- Programmation-II/src/Project 013 (The Tortoise and the Hare)/Main.cpp
+++ Programmation-II/src/Project 013 (The Tortoise and the Hare)/Main.cpp
@@ -1,30 +1,30 @@
-// The Tortoise and the Hare
+// The Tortoise and the Hare 
 // Main.cpp (Main function | Program execution begins and ends there) 
 
 /*
  EXERCICE 8.12 PSEUDOCODE
 
- - utilisation de nombre aléatoire pour recréation de la simulation
- - créer un array de 70 cases
- - chacune des 70 cases représente une position le long du parcours de course
- - la ligne d'arrivée est à la case 70
- - le premier a atteindre la case 70 reçois : "pail of fresh carrots and lettuce"
- - Le parcours serpente le long d'une montagne glissante, donc parfois les prétendants perdent du terrain.
+ - utilisation de nombre alÃ©atoire pour recrÃ©ation de la simulation
+ - crÃ©er un array de 70 cases
+ - chacune des 70 cases reprÃ©sente une position le long du parcours de course
+ - la ligne d'arrivÃ©e est Ã  la case 70
+ - le premier a atteindre la case 70 reÃ§ois : "pail of fresh carrots and lettuce"
+ - Le parcours serpente le long d'une montagne glissante, donc parfois les prÃ©tendants perdent du terrain.
  - Il y a une horloge qui tic-tac une fois par seconde.
- - Le programme doit utiliser les fonctions "moveTortoise" et "moveHare" pour ajuster la position des animaux selon les règles de la Figure 8.18
- - Ces fonctions doivent utiliser des pointeurs passe-par-référence pour modifier la position de la tortue et du lièvre.
- - Utilisez des variables pour garder une trace des positions des animaux (les numéros de position vont de 1 à 70).
- - Commencer chaque animal à la position 1 (c'est-à-dire la "starting gate")
- - Si un animal glisse à gauche avant la case 1, déplacez l'animal vers la case 1.
+ - Le programme doit utiliser les fonctions "moveTortoise" et "moveHare" pour ajuster la position des animaux selon les rÃ¨gles de la Figure 8.18
+ - Ces fonctions doivent utiliser des pointeurs passe-par-rÃ©fÃ©rence pour modifier la position de la tortue et du liÃ¨vre.
+ - Utilisez des variables pour garder une trace des positions des animaux (les numÃ©ros de position vont de 1 Ã  70).
+ - Commencer chaque animal Ã  la position 1 (c'est-Ã -dire la "starting gate")
+ - Si un animal glisse Ã  gauche avant la case 1, dÃ©placez l'animal vers la case 1.
 
  PERCENTAGE (FIG 8.18)
- - Générez les pourcentages de la Figure 8.18 en produisant un entier aléatoire i dans la plage 1 <= i <= 10
+ - GÃ©nÃ©rez les pourcentages de la Figure 8.18 en produisant un entier alÃ©atoire i dans la plage 1 <= i <= 10
  - Pour la tortue, effectuez une :
    > "fast plod"   quand 1 <= i <= 5  (50%) actual move : 3 case a droite
    > "slip"        quand 6 <= i <= 7  (20%) actual move : 6 case a GAUCHE
    > "slow plod"   quand 8 <= i <= 10 (30%) actual move : 1 case a droite
 
- -  Utilisez une technique similaire pour déplacer le lièvre :
+ -  Utilisez une technique similaire pour dÃ©placer le liÃ¨vre :
    > "sleep"       quand 1 <= i <= 2  (20%) actual move : ne bouge pas
    > "big hop"     quand 3 <= i <= 4  (20%) actual move : 9 case a droite
    > "big slip"    quand 5 <= i <= 5  (10%) actual move : 12 case a GAUCHE
@@ -36,16 +36,16 @@ BEGIN THE RACE BY DISPLAYING :
 BANG !!!!!
 AND THEY'RE OFF !!!!!
 
-- Pour chaque tick de l'horloge (c'est-à-dire chaque itération d'une boucle), affichez une ligne à 70 positions indiquant la lettre "T" en position de tortue (tortoise) et la lettre "H" en position de lièvre (hare).
-- Parfois, les concurrents atterrissent sur la même case. Dans ce cas, la tortue mord le lièvre et votre programme devrait afficher "OUCH!!!" commençant à cette position.
-- Toutes les positions autres que le "T" , le "H" ou "OUCH!!!" (en cas d'égalité) doit être vide.
+- Pour chaque tick de l'horloge (c'est-Ã -dire chaque itÃ©ration d'une boucle), affichez une ligne Ã  70 positions indiquant la lettre "T" en position de tortue (tortoise) et la lettre "H" en position de liÃ¨vre (hare).
+- Parfois, les concurrents atterrissent sur la mÃªme case. Dans ce cas, la tortue mord le liÃ¨vre et votre programme devrait afficher "OUCH!!!" commenÃ§ant Ã  cette position.
+- Toutes les positions autres que le "T" , le "H" ou "OUCH!!!" (en cas d'Ã©galitÃ©) doit Ãªtre vide.
 
 WINNING
-- Après avoir affiché chaque ligne, testez si l'un ou l'autre des animaux a atteint ou dépassé la case 70.
+- AprÃ¨s avoir affichÃ© chaque ligne, testez si l'un ou l'autre des animaux a atteint ou dÃ©passÃ© la case 70.
 - Si c'est le cas, affichez le gagnant et terminer la simulation.
 - Si la tortue gagne, affichez "TORTOISE WINS!!! YAY!!!" | "LA TORTUE GAGNE!!! YAY!!!"
-- Si le lièvre gagne, affichez "Hare wins. Yuch." | "Le lièvre gagne. Yuch."
-- Si les deux animaux gagnent sur le même tic d'horloge, afficher "It's a tie."
+- Si le liÃ¨vre gagne, affichez "Hare wins. Yuch." | "Le liÃ¨vre gagne. Yuch."
+- Si les deux animaux gagnent sur le mÃªme tic d'horloge, afficher "It's a tie."
 - Si aucun animal ne gagne, refaites la boucle pour simuler le prochain tic-tac de l'horloge.
 */
 
