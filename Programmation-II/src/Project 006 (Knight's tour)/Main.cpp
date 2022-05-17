a9597ae1b9321d5f7822832fb345145133cb049d
Alexandre Perreault <epsilons@hotmail.com>
2022-05-16 23:20:39 -0400

    Add files via upload

---
+++ Programmation-II/src/Project 006 (Knight's tour)/Main.cpp
@@ -0,0 +1,15 @@
+// Knight's tour
+// Main.cpp (Main function | Program execution begins and ends there) 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+	system("pause");
+
+	return 0;
+}
