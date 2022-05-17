2026a58b9334c70e24505ee6c8ff4cc672cdd6a9
Alexandre Perreault <epsilons@hotmail.com>
2022-05-16 23:03:45 -0400

    Add files via upload

---
+++ Prototypage/Project 006 (Enable, disable and find game object)/FindGameObject.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindGameObject : MonoBehaviour
+{
+    // Declare variables of type GameObject
+    public GameObject myCube; 
+    public GameObject mySphere;
+    public GameObject myCylinder;
+    public GameObject[] MyObjectsArray;
+
+    void Awake()
+    {
+        // Find a GameObject by name
+        myCube = GameObject.Find("Cube");
+
+        // Find a GameObject by name within a parent/child tree
+        mySphere = GameObject.Find("All my objects/Find by name/Sphere");
+
+        // Find a GameObject by tag
+        // Tag are defined and assigned for each GameObject individually in the inspector
+        myCylinder = GameObject.FindWithTag("CylinderTag");
+
+        // Find multiples GameObjects with the same "Tag"
+        // Tag are defined and assigned for each GameObject individually in the inspector
+        MyObjectsArray = GameObject.FindGameObjectsWithTag ("GameObjectTag");
+    }
+}
