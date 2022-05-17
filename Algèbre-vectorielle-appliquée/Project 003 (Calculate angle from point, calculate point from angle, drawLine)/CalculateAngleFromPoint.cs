7c4390730c64bb79804b0b308f40bda7a5d9f41d
Alexandre Perreault <epsilons@hotmail.com>
2022-05-16 22:56:13 -0400

    Add files via upload

---
+++ Algèbre-vectorielle-appliquée/Project 003 (Calculate angle from point, calculate point from angle, drawLine)/CalculateAngleFromPoint.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculateAngleFromPoint : MonoBehaviour
+{
+    [SerializeField] private Vector2 position = default;
+    [SerializeField] private GameObject MySphere;
+
+    private void Update()
+    {
+        MySphere.transform.position = position;
+
+        float angleInRad = CalculateAngleWithSin(position);
+        float angleInDegree = RadToDeg(angleInRad);
+
+        Debug.Log("Angle in degree: " + angleInDegree);
+    }
+
+    private float CalculateAngleWithSin(Vector2 p)
+    {
+        float sinAngle = p.y / hypotenuse(p.x, p.y);
+        float angle = Mathf.Asin(sinAngle);
+
+        return angle;
+    }
+
+    private float CalculateAngleWithTan(Vector2 p)
+    {
+        float tanAngle = p.y / hypotenuse(p.x, p.y);
+        float angle = Mathf.Atan(tanAngle);
+
+        return angle;
+    }
+
+    private float hypotenuse(float dx, float dy)
+    {
+        float magn_2 = dx * dx + dy * dy;
+        return Mathf.Sqrt(magn_2);
+    }
+
+    private float hypotenuse(Vector2 dp)
+    {
+        float magn_2 = dp.x * dp.x + dp.y * dp.y;
+        return Mathf.Sqrt(magn_2);
+    }
+
+    private float RadToDeg(float angleInRadian)
+    {
+        return (angleInRadian * 180f / Mathf.PI);
+    }
+}
---
+++ Algèbre-vectorielle-appliquée/Project 003 (Calculate angle from point, calculate point from angle, drawLine)/DrawLine.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{   
+    [SerializeField] Vector2 startPoint;
+    [SerializeField] Vector2 endPoint;
+
+    private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+
+        if (lineRenderer == null) 
+        {
+            Debug.LogWarning("<DrawLine> : Pas de reference pour le lineRenderer-> la composante est surement manquante!");
+        }
+    }
+
+    void Update()
+    {
+        SetLine(startPoint, endPoint);
+    }
+    
+    private void SetLine(Vector2 start, Vector2 end)
+    {
+        if (lineRenderer == null) { return; }
+
+        lineRenderer.widthMultiplier = 0.25f;
+        lineRenderer.positionCount = 2;
+
+        lineRenderer.SetPosition(0, start);
+        lineRenderer.SetPosition(1, end);
+    }
+}
