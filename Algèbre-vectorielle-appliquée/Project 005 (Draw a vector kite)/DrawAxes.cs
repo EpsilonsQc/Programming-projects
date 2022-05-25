7aaf5b996623e86842cd8ec4fb927c2c301caf62
Alexandre Perreault <epsilons@hotmail.com>
2022-05-25 16:58:35 -0400

    Add files via upload

---
+++ Algèbre-vectorielle-appliquée/Project 005 (Draw a vector kite)/DrawAxes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MyDebug;
+
+public class DrawAxes : MonoBehaviour
+{
+    [SerializeField] float lineWidth;
+
+    [SerializeField] Vector2 axeX_start;
+    [SerializeField] Vector2 axeX_end;
+
+    [SerializeField] Vector2 axeY_start;
+    [SerializeField] Vector2 axeY_end;
+
+    private void Start()
+    {
+        Segment.DrawNewSegment(axeX_start, axeX_end, lineWidth, Color.red);
+        Segment.DrawNewSegment(axeY_start, axeY_end, lineWidth, Color.green);
+    }
+}
---
+++ Algèbre-vectorielle-appliquée/Project 005 (Draw a vector kite)/DrawKite.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MyDebug;
+
+public class DrawKite : MonoBehaviour
+{
+    // Line width
+    [Header("LINE WIDTH")]
+    [SerializeField] private float lineWidth = 0.5f;
+
+    // Kite origin
+    Vector2 kiteOrigin = new Vector2(20f, 40f);
+
+    // P1
+    static float p1y = Mathf.Cos(DegToRad(56)) * 32;
+    static float p1x = Mathf.Sqrt((32 * 32 - p1y * p1y));
+    Vector2 p1 = new Vector2(Mathf.Abs(p1x),Mathf.Abs(p1y));
+
+    // P2
+    static float p2x = Mathf.Cos(DegToRad(10)) * 32;
+    static float p2y = Mathf.Sqrt((32 * 32 - p2x * p2x));
+    Vector2 p2 = new Vector2(Mathf.Abs(p2x),Mathf.Abs(p2y));
+
+    // P3
+    static float p3x = Mathf.Cos(DegToRad(22)) * 40;
+    static float p3y = Mathf.Sqrt((40 * 40 - p3x * p3x));
+    Vector2 p3 = new Vector2(Mathf.Abs(p3x),Mathf.Abs(p3y));
+
+    // Convert degree to radiant
+    static float DegToRad(float degree)
+    {
+        float radiant = degree * 3.1416f / 180f;
+        return radiant;
+    }
+
+    // Calculate angle, direction of vectors and draw kite segments
+    void RenderKite()
+    {
+        // Blue line
+        Segment.DrawNewSegment(kiteOrigin, p1 + kiteOrigin, lineWidth, Color.blue);
+        Segment.DrawNewSegment(kiteOrigin, p2 + kiteOrigin, lineWidth, Color.blue);
+        Segment.DrawNewSegment(p3 + kiteOrigin, p2 + kiteOrigin, lineWidth, Color.blue);
+        Segment.DrawNewSegment(p3 + kiteOrigin, p1 + kiteOrigin, lineWidth, Color.blue);
+
+        // Red line
+        Segment.DrawNewSegment(kiteOrigin, p3 + kiteOrigin, lineWidth, Color.red);
+        Segment.DrawNewSegment(p1 + kiteOrigin, p2 + kiteOrigin, lineWidth, Color.red);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(p1);
+        Debug.Log(p2);
+        Debug.Log(p3);
+        RenderKite();
+    }
+}
