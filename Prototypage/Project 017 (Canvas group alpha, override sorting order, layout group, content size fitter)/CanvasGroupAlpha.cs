eca0a0bf551e1514c048b2e7b03e2302bb07d467
Alexandre Perreault <epsilons@hotmail.com>
2022-06-06 17:18:05 -0400

    Add files via upload

---
+++ Prototypage/Project 017 (Canvas group alpha, override sorting order, layout group, content size fitter)/CanvasGroupAlpha.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasGroupAlpha : MonoBehaviour
+{
+    [SerializeField] CanvasGroup myCanvasGroup;
+    [SerializeField] float myTimer = 2f;
+    [SerializeField] float t;
+
+    void Awake()
+    {
+        myCanvasGroup = gameObject.GetComponent<CanvasGroup>();
+    }
+
+    void Update()
+    {
+        // With PingPong
+        // myCanvasGroup.alpha = (Mathf.PingPong(Time.time / myTimer, 1));
+
+        // With Lerp
+        t += myTimer * Time.deltaTime;
+        myCanvasGroup.alpha = Mathf.Lerp(1, 0 , t);
+    }
+}
