5c27c768a28bc6dfa3a7963165ff24345b16d8aa
Alexandre Perreault <epsilons@hotmail.com>
2022-06-09 00:42:49 -0400

    Add files via upload

---
+++ Algèbre-vectorielle-appliquée/Project 007 (Easing functions, interpolation)/EasingFunctions.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasingFunctions : MonoBehaviour
+{
+    public enum InterpolationType
+    {
+        Linear,
+        EaseIn,
+        EaseOut,
+        EaseInOut,
+        EaseOutBounce
+    }
+
+    public enum MethodType
+    {
+        Default,
+        Loop,
+        PingPong
+    }
+
+    [Header("TYPE")]
+    [SerializeField] InterpolationType interpolationType = InterpolationType.Linear;
+    [SerializeField] MethodType methodType = MethodType.Loop;
+    [Space(10)]
+
+    [Header("OFFSET POSITION")]
+    [SerializeField] Vector3 offsetPosition;
+    [Space(10)]
+
+    [Header("MAX DURATION")]
+    [SerializeField] float maxDuration;
+    [Space(10)]
+
+    [Header("TARGET COLOR")]
+    [SerializeField] Color targetColor = Color.red;
+
+    Vector3 startPosition;
+    Vector3 endPosition;
+    Color spriteColor;
+
+    SpriteRenderer spriteRenderer;
+
+    private float currentDuration = 0f;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteColor = spriteRenderer.color;
+
+        startPosition = transform.position;
+
+        maxDuration = 2.5f;
+    }
+
+    void Update()
+    {
+        endPosition = startPosition + offsetPosition;
+
+        UpdateInterpolation(interpolationType, methodType);
+    }
+
+    void UpdateInterpolation(InterpolationType interpolationType, MethodType methodType)
+    {
+        Vector3 position;
+
+        currentDuration += Time.deltaTime;
+        float currentTime = UpdateTime();
+
+        switch (interpolationType)
+        {
+            case InterpolationType.Linear:
+                position = Lerp(startPosition, endPosition, currentTime);
+                break;
+            case InterpolationType.EaseIn:
+                position = EaseIn(startPosition, endPosition, currentTime);
+                break;
+            case InterpolationType.EaseOut:
+                position = EaseOut(startPosition, endPosition, currentTime);
+                break;
+            case InterpolationType.EaseInOut:
+                position = EaseInOut(startPosition, endPosition, currentTime);
+                break;
+            case InterpolationType.EaseOutBounce:
+                position = EaseOutBounce(startPosition, endPosition, currentTime);
+                break;
+            default:
+                position = Vector3.zero;
+                break;
+        }
+
+        spriteColor = Color.Lerp(Color.white, targetColor, currentTime);
+        spriteRenderer.color = spriteColor;
+
+        if (currentTime <= 1f) 
+        {
+            transform.position = position;
+        }
+    }
+
+    private float UpdateTime()
+    {
+        float time = currentDuration / maxDuration;
+
+        switch (methodType)
+        {
+            case MethodType.Loop:
+                if (time >= 1f)
+                {
+                    currentDuration = 0f;
+                    time = 0f;
+                }
+                break;
+            case MethodType.PingPong:
+                time = Mathf.Sin(time * Mathf.PI) / 2 + 0.5f;
+                break;
+            default:
+                break;
+        }
+        return time;
+    }
+
+    #region INTERPOLATION Functions
+    // Lerp (Float)
+    float Lerp(float start, float end, float t)
+    {
+        float result = start + (end - start) * t;
+        return result;
+    }
+
+    // Lerp (Vector 2)
+    Vector2 Lerp(Vector2 start, Vector2 end, float t)
+    {
+        Vector2 result = start + (end - start) * t;
+        return result;
+    }
+
+    // Lerp (Vector 3)
+    Vector3 Lerp(Vector3 start, Vector3 end, float t)
+    {
+        Vector3 result = start + (end - start) * t;
+        return result;
+    }
+
+    // Ease In (float)
+    float EaseIn(float start, float end, float t)
+    {
+        float result = start + (end - start) * t * t;
+        return result;
+    }
+
+    // Ease In (Vector 3)
+    Vector3 EaseIn(Vector3 start, Vector3 end, float t)
+    {
+        Vector3 result = start + (end - start) * t * t;
+        return result;
+    }
+
+    // Ease Out (float)
+    private float EaseOut(float start, float end, float t)
+    {
+        float result = start - (end - start) * t * (t - 2);
+        return result;
+    }
+    
+    // East Out (Vector 3)
+    private Vector3 EaseOut(Vector3 start, Vector3 end, float t)
+    {
+        Vector3 result = start - (end - start) * t * (t - 2);
+        return result;
+    }
+
+    // Ease In Out (Float)
+    private float EaseInOut(float start, float end, float t)
+    {
+        float result = start - (end - start) * (0.5f * Mathf.Cos(Mathf.PI * t) - 0.5f);
+        return result;
+    }
+
+    // Ease In Out (Vector 3)
+    private Vector3 EaseInOut(Vector3 start, Vector3 end, float t)
+    {
+        Vector3 result = start - (end - start) * (0.5f * Mathf.Cos(Mathf.PI * t) - 0.5f);
+        return result;
+    }
+
+    // Ease Out Bounce
+    private Vector3 EaseOutBounce(Vector3 start, Vector3 end, float x)
+    {
+        const float n1 = 7.5625f;
+        const float d1 = 2.75f;
+        float t;
+
+        if (x < 1 / d1)
+        {
+            t = n1 * x * x;
+        }
+        else if (x < 2 / d1)
+        {
+            t = n1 * (x -= 1.5f / d1) * x + 0.75f;
+        }
+        else if (x < 2.5 / d1)
+        {
+            t = n1 * (x -= 2.25f / d1) * x + 0.9375f;
+        }
+        else
+        {
+            t = n1 * (x -= 2.625f / d1) * x + 0.984375f;
+        }
+        return start + (end - start) * t;
+    }
+    #endregion
+}
---
+++ Algèbre-vectorielle-appliquée/Project 007 (Easing functions, interpolation)/Interpolation.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interpolation : MonoBehaviour
+{
+    [Header("VECTORS")]
+    [SerializeField] Vector3 startPosition;
+    [SerializeField] Vector3 endPosition;
+    [Space(10)]
+
+    [Header("VARIABLES")]
+    [SerializeField] float time; // duration in seconds of the interpolation
+    [SerializeField] float deltaTimeCounter;
+    [Space(10)]
+    [SerializeField] float distanceCoveredPercentage; // Percentage of distance covered by the interpolation
+    [SerializeField] float interpolationRatio; // This parameter is clamped to the range [0, 1]
+    [Space(10)]
+    [SerializeField] bool loopLerp;
+    [SerializeField] bool pauseLerp;
+    [SerializeField] bool resetLerp;
+
+    void Awake()
+    {
+        startPosition = transform.position;
+        endPosition = new Vector3(100, 0 ,0);
+
+        time = 2.5f; // duration in seconds of the interpolation between two points
+        deltaTimeCounter = 0f;
+
+        loopLerp = false;
+        pauseLerp = true;
+        resetLerp = false;
+    }
+
+    void Update()
+    {
+        if(deltaTimeCounter <= time && pauseLerp == false && resetLerp == false)
+        {
+            deltaTimeCounter += Time.deltaTime;
+            interpolationRatio = deltaTimeCounter / time;
+            distanceCoveredPercentage = (interpolationRatio * 100);
+
+            if(interpolationRatio <= 1f && pauseLerp == false && resetLerp == false)
+            {
+                transform.position = LerpWithVector(startPosition, endPosition, interpolationRatio);
+            }
+        }
+
+        if (interpolationRatio >= 1f && loopLerp == true)
+        {
+            transform.position = startPosition;
+            deltaTimeCounter = 0f;
+            resetLerp = false;
+        }
+
+        if(resetLerp == true)
+        {
+            transform.position = startPosition;
+            deltaTimeCounter = 0f;
+            resetLerp = false;
+        }
+    }
+
+    // Linear interpolation (also known as "lerp"). Move an object gradually between two points
+    Vector3 LerpWithVector(Vector3 start, Vector3 end, float t)
+    {
+        return (start + (end - start) * t);
+    }
+}
