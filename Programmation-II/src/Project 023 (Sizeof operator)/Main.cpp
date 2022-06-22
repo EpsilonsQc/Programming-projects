5e92553e69186267427b490e87f50ebc3f11cdc0
Alexandre Perreault <epsilons@hotmail.com>
2022-06-22 00:46:28 -0400

    Add files via upload

---
+++ Programmation-II/src/Project 023 (Sizeof operator)/Main.cpp
@@ -0,0 +1,15 @@
+// Sizeof operator
+// Main.cpp (Main function | Program execution begins and ends there) 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+	system("pause");
+
+	return 0;
+}
