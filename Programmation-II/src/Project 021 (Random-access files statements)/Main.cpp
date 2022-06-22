1fbcbc37293155875f36101cb86f427f3e8e5bb4
Alexandre Perreault <epsilons@hotmail.com>
2022-06-22 00:44:09 -0400

    Add files via upload

---
+++ Programmation-II/src/Project 021 (Random-access files statements)/Main.cpp
@@ -0,0 +1,15 @@
+// Random-access files statements
+// Main.cpp (Main function | Program execution begins and ends there) 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+	system("pause");
+
+	return 0;
+}
