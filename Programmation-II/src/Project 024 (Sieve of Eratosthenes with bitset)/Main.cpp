fd269f55d20afa03284332222ca1d2816db3523a
Alexandre Perreault <epsilons@hotmail.com>
2022-06-22 00:47:38 -0400

    Add files via upload

---
+++ Programmation-II/src/Project 024 (Sieve of Eratosthenes with bitset)/Main.cpp
@@ -0,0 +1,15 @@
+// Sieve of Eratosthenes with bitset
+// Main.cpp (Main function | Program execution begins and ends there) 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+	system("pause");
+
+	return 0;
+}
