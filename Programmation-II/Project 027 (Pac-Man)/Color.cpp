9de3a32fd04b45528061148bcde841e54b1113ec
Alexandre Perreault <epsilons@hotmail.com>
2022-06-30 00:41:26 -0400

    Delete Programmation-II directory

--- Programmation-II/Project 027 (Pac-Man)/Color.cpp
+++
@@ -1,9 +0,0 @@
-#include "Color.h"
-
-Color Color::Black = Color(0, 0, 0);
-Color Color::Yellow = Color(255, 254, 84);
-Color Color::Blue = Color(0, 0, 255);
-Color Color::White = Color(255, 255, 255);
-Color Color::Orange = Color(238, 110, 46);
-Color Color::Gray = Color(112, 112, 112);
-Color Color::Red = Color(255, 0, 0);
