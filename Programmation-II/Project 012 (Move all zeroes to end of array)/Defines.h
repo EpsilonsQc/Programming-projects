a4bd99b7e20f1f38b3af1c2a5a0238d4d3a9b1c7
Alexandre Perreault <epsilons@hotmail.com>
2022-06-30 00:42:58 -0400

    Delete Programmation-II directory

--- Programmation-II/Project 012 (Move all zeroes to end of array)/Defines.h
+++
@@ -1,7 +0,0 @@
-// Move all zeroes to end of array
-// Defines.h (Definition file | Constant variables (read-only) that cannot be updated or altered anywhere)
-
-#pragma once
-
-// Define the size of my array
-static const size_t arraySize = 9;
